cdc900fdd624114361a35174b12afb92fbe45e32
zihan zhou <61923893+zzhzoe@users.noreply.github.com>
2021-11-21 03:03:28 -0800

    Create exercise8demo.py

---
+++ exercise8demo.py
@@ -0,0 +1,18 @@
+# author: Tiffany Timbers
+# date: 2020-01-15
+
+"""This script prints out docopt args.
+Usage: demo.py <arg1> --arg2=<arg2> [<arg4>] [--arg3=<arg3>]
+
+Options:
+<arg1>             Takes any value (this is a required positional argument)
+--arg2=<arg2>     Takes any value (this is a required option)
+[<arg4>]           Takes any value (this is an optional positional argument)
+[--arg3=<arg3>]   Takes any value (this is an optional option)
+""" 
+
+from docopt import docopt
+opt = docopt(__doc__)
+print(opt)
+print(type(opt))
+print(opt["<arg4>"])
