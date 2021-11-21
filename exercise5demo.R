1b9f67958feb2e896e6c4097fa21ea7b74bace14
zihan zhou <61923893+zzhzoe@users.noreply.github.com>
2021-11-21 02:37:19 -0800

    add print and argument

--- exercise5demo.R
+++ exercise5demo.R
@@ -3,11 +3,12 @@
 
 
 "This script prints out docopt args.
-Usage: demo.R <arg1> --arg2=<arg2> [--arg3=<arg3>]
+Usage: demo.R <arg1> --arg2=<arg2> [<arg4>] [--arg3=<arg3>]
 
 Options:
 <arg>             Takes any value (this is a required positional argument)
 --arg2=<arg2>     Takes any value (this is a required option)
+[<arg4>]          Takes any value (this is a required positional argument)
 [--arg3=<arg3>]   Takes any value (this is an optional option)
 " -> doc
 
@@ -15,3 +16,4 @@ library(docopt)
 opt <- docopt(doc)
 print(opt)
 print(typeof(opt))
+print(opt$arg4) 
