6d3fdd388fd33c4d8f5cd03679e1546878e914d3
zihan zhou <61923893+zzhzoe@users.noreply.github.com>
2021-11-21 02:35:21 -0800

    back to original

--- exercise5demo.R
+++ exercise5demo.R
@@ -3,23 +3,15 @@
 
 
 "This script prints out docopt args.
-Usage: demo.R <arg1> --arg2=<arg2> [<arg4>] [--arg3=<arg3>] 
+Usage: demo.R <arg1> --arg2=<arg2> [--arg3=<arg3>]
 
 Options:
 <arg>             Takes any value (this is a required positional argument)
 --arg2=<arg2>     Takes any value (this is a required option)
-[<arg4>]          Takes any value (this is a required positional argument)
 [--arg3=<arg3>]   Takes any value (this is an optional option)
 " -> doc
 
 library(docopt)
 opt <- docopt(doc)
-
-main <- function(){
-  print(opt)
-  print(typeof(opt))
-  print(opt$arg4)
-  print(opt$arg3)
-}
-
-main()
+print(opt)
+print(typeof(opt))
