63e181a5920a6434900082854029f3cee51e412e
zzhzoe <61923893+zzhzoe@users.noreply.github.com>
2021-11-21 02:11:23 -0800

    Create exercise5demo.R

---
+++ exercise5demo.R
@@ -0,0 +1,24 @@
+# author: Tiffany Timbers -modified by zihan zhou
+# date: 2020-01-15 -modified on 2021/11/21
+
+
+"This script prints out docopt args.
+Usage: demo.R <arg1> --arg2=<arg2> [<arg4>] [--arg3=<arg3>] 
+
+Options:
+<arg>             Takes any value (this is a required positional argument)
+--arg2=<arg2>     Takes any value (this is a required option)
+[<arg4>]          Takes any value (this is a required positional argument)
+[--arg3=<arg3>]   Takes any value (this is an optional option)
+" -> doc
+
+library(docopt)
+opt <- docopt(doc)
+
+main <- function(){
+  print(opt)
+  print(typeof(opt))
+  print(opt$arg4)
+}
+
+main()
