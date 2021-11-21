cdf6d0917e02196e98efd25e7d1d2cb12d1c5fd3
zihan zhou <61923893+zzhzoe@users.noreply.github.com>
2021-11-21 02:40:09 -0800

    add main

--- exercise5demo.R
+++ exercise5demo.R
@@ -14,6 +14,11 @@ Options:
 
 library(docopt)
 opt <- docopt(doc)
-print(opt)
-print(typeof(opt))
-print(opt$arg4) 
+
+main <- function(){
+  print(opt)
+  print(typeof(opt))
+  print(opt$arg3)
+}
+
+
